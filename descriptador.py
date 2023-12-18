c0e828a3031180af702bde66971516b8f26e052b
Alan Silva <86873795+alansilv4@users.noreply.github.com>
2023-12-18 20:36:41 -0300

    Update and rename decrypter.py to descriptador.py

--- descriptador.py
+++ descriptador.py
@@ -1,8 +1,8 @@
 import os
 import pyaes
 
-## abrir o arquivo criptografado
-file_name = "teste.txt.ransomwaretroll"
+## abre o arquivo criptografado
+file_name = "desafio.txt.perdeumenor"
 file = open(file_name, "rb")
 file_data = file.read()
 file.close()
@@ -12,11 +12,11 @@ key = b"testeransomwares"
 aes = pyaes.AESModeOfOperationCTR(key)
 decrypt_data = aes.decrypt(file_data)
 
-## remover o arquivo criptografado
+## remove o arquivo criptografado
 os.remove(file_name)
 
-## criar o arquivo descriptografado
-new_file = "teste.txt"
+## cria o arquivo descriptografado
+new_file = "desafio.txt"
 new_file = open(f'{new_file}', "wb")
 new_file.write(decrypt_data)
 new_file.close()
